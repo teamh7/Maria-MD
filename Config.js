122a8492f8c43b6553df2eebb110603037f6e7b8
teamh7 <149703017+teamh7@users.noreply.github.com>
2024-04-08 20:24:34 +0530

    Update Config.js

--- Config.js
+++ Config.js
@@ -1,61 +1,121 @@
-const fs = require('fs')
-const chalk = require('chalk')
-
-//contact details
-global.ownernumber = ['7015947242']
-global.ownername = "TEAM H7"
-global.ytname = "YT: AyushBotz.inc"
-global.socialm = "GitHub: Ayushpandey023"
-global.location = "Indian"
-
-global.ownername = "TEAMH7" //owner name
-global.botname = '𝐌𝐀𝐑𝐈𝐀-𝐌𝐃' //name of the bot
-
-//sticker details
-global.stickername = '💙𓆩‎✘AʏᴜSʜ✘𓆪_✰'
-global.packname = 'Sticker By'
-global.author = 'Maria Bot'
-//console view/theme
-global.themeemoji = '🧩'
-global.wm = "Ayush botz inc."
-
-//theme link
-global.link = 'https://whatsapp.com/channel/0029VaImo5ZG3R3qjKOdyr1I'
-
-//custom prefix
-global.prefa = ['.']
-
-//false=disable and true=enable
-global.welcome = false //auto welcome
-global.autoRecording = false //auto recording
-global.autoTyping = false //auto typing
-global.autorecordtype = false //auto typing + recording
-global.autoread = false //auto read messages
-global.autobio = false //auto update bio
-global.anti212 = true //auto block +212
-global.autoread_status = false //auto view status/story
-
-
-
-//reply messages
-global.mess = {
-    done: '*here you go!* \n\n*🎀 Maria Bot 🎀*\n\n*🧩 Bot link:* \nhttps://github.com/AYUSH-PANDEY023/Maria-MD\n',
-    prem: '*This feature can be used by premium user only*',
-    admin: '*This feature can be used by admin only*',
-    botAdmin: '*This feature can only be used when the bot is a group admin* ',
-    owner: '*This feature can be used by owner only*',
-    group: '*This feature is only for groups*',
-    private: '*This feature is only for private chats*',
-    wait: '*In process...* ',    
-    error: '*Error!*',
-}
-
-global.thumb = fs.readFileSync('./Gallery/thumb.jpg')
-
-let file = require.resolve(__filename)
-fs.watchFile(file, () => {
-    fs.unwatchFile(file)
-    console.log(chalk.redBright(`Update'${__filename}'`))
-    delete require.cache[file]
-    require(file)
-})
+G146	G146							* myContacts	Mobile	+91 79769 75568
+G147	G147							* myContacts	Mobile	+91 79765 31476
+G148	G148							* myContacts	Mobile	+91 79008 01316 ::: +91 79061 45214
+G149	G149							* myContacts	Mobile	+91 78917 96342
+G15	G15							* myContacts	Mobile	62833 80332
+G150	G150							* myContacts	Mobile	+91 788 799 0676
+G151	G151							* myContacts	Mobile	+91 78788 12739
+G152	G152							* myContacts	Mobile	+91 78781 97944
+G155	G155							* myContacts	Mobile	+91 78777 16341
+G157	G157						https://lh3.googleusercontent.com/contacts/AG6tpzFw5Re8jKDEm0otxUEiEHSuktUTok1GxHzGdR4JYNDYgJLa14Jp	* myContacts	Mobile	+91 78762 06621
+G158	G158							* myContacts	Mobile	+91 78691 59128
+G159	G159							* myContacts	Mobile	+91 78387 23208
+G16	G16							* myContacts	Mobile	6295 992 194
+G160	G160							* myContacts	Mobile	+91 78383 74494
+G161	G161							* myContacts	Mobile	+91 78359 48471
+G162	G162						https://lh3.googleusercontent.com/contacts/AG6tpzEzN3N16BNyNCiLQ1TegB2BzzIn4nqRT4bHo7VXRCsb_zejUeDV	* myContacts	Mobile	+91 78274 11755
+G163	G163							* myContacts	Mobile	+91 78219 13295
+G164	G164							* myContacts	Mobile	+91 81144 80048
+G165	G165							* myContacts	Mobile	+91 81075 58991
+G167	G167							* myContacts	Mobile	+91 80533 43550 ::: +91 80815 29024
+G168	G168							* myContacts	Mobile	8053343550
+G169	G169							* myContacts	Mobile	+91 80529 88943
+G17	G17							* myContacts	Mobile	63505 21097
+G172	G172							* myContacts	Mobile	+91 80031 07357
+G173	G173							* myContacts	Mobile	+91 80005 76714
+G174	G174							* myContacts	Mobile	+91 79968 29399
+G175	G175							* myContacts	Mobile	+91 79888 58556
+G176	G176							* myContacts	Mobile	+91 79867 26970
+G177	G177							* myContacts	Mobile	+91 79863 33321
+G179	G179							* myContacts	Mobile	+91 79837 74086
+G18	G18							* myContacts	Mobile	63505 50998
+G183	G183							* myContacts	Mobile	+91 80948 85230
+G21	G21							* myContacts	Mobile	+91 62031 22514
+G22	G22							* myContacts	Mobile	+91 62801 88565
+G23	G23							* myContacts	Mobile	+91 78272 59267
+G24	G24							* myContacts	Mobile	+91 76786 96801
+G25	G25							* myContacts	Mobile	+91 75684 44689
+G26	G26							* myContacts	Mobile	+91 75574 44221
+G27	G27							* myContacts	Mobile	+91 75053 79897
+G28	G28							* myContacts	Mobile	+91 74192 93646
+G29	G29							* myContacts	Mobile	+91 72063 84492
+G30	G30							* myContacts	Mobile	+91 70245 47552
+G32	G32							* myContacts	Mobile	+91 70234 45188
+G33	G33							* myContacts	Mobile	+91 99968 66351
+G34	G34							* myContacts	Mobile	+91 99924 67626
+G35	G35							* myContacts	Mobile	+91 99918 84156
+G36	G36							* myContacts	Mobile	+91 99889 63492
+G37	G37							* myContacts	Mobile	+91 99713 15528
+G38	G38							* myContacts	Mobile	+91 98967 41714
+G40	G40							* myContacts	Mobile	+91 98966 53294
+G41	G41							* myContacts	Mobile	+91 98880 80512
+G42	G42							* myContacts	Mobile	+91 98716 53451
+G43	G43							* myContacts	Mobile	+91 98130 50037
+G44	G44							* myContacts	Mobile	+91 98118 70114
+G45	G45							* myContacts	Mobile	+91 98030 70394
+G46	G46							* myContacts	Mobile	+91 97619 52457
+G47	G47							* myContacts	Mobile	+91 97614 17032
+G48	G48							* myContacts	Mobile	+91 97295 02933
+G49	G49							* myContacts	Mobile	+91 97296 03281
+G52	G52							* myContacts	Mobile	+91 97619 52457
+G53	G53							* myContacts	Mobile	+91 97614 17032
+G54	G54							* myContacts	Mobile	+91 97296 03281
+G55	G55							* myContacts	Mobile	+91 97295 02933
+G56	G56							* myContacts	Mobile	+91 97172 21695
+G57	G57						https://lh3.googleusercontent.com/contacts/AG6tpzF1yKU9m8nN7FFDErNbXLD7ewt73xywoaq7KLCUbBMo-pL21mN1	* myContacts	Mobile	+91 96906 50303
+G58	G58							* myContacts	Mobile	+91 96905 76165
+G59	G59							* myContacts	Mobile	+91 70780 78768
+G60	G60							* myContacts	Mobile	+91 70699 25710
+G61	G61							* myContacts	Mobile	+91 70678 58211
+G62	G62							* myContacts	Mobile	+91 70657 80927
+G63	G63							* myContacts	Mobile	+91 70587 40808
+G65	G65							* myContacts	Mobile	+91 70562 52601
+G66	G66							* myContacts	Mobile	+91 70489 27519
+G67	G67							* myContacts	Mobile	+91 70429 12645
+G68	G68							* myContacts	Mobile	+91 70372 81076
+G69	G69							* myContacts	Mobile	+91 70277 55017
+G71	G71							* myContacts	Mobile	+91 70240 83918
+G72	G72							* myContacts	Mobile	+91 70183 16480
+G73	G73							* myContacts	Mobile	+91 70179 25521
+G74	G74							* myContacts	Mobile	+91 70175 82850
+G75	G75							* myContacts	Mobile	+91 70173 74212
+G76	G76							* myContacts	Mobile	+91 70141 70903
+G77	G77							* myContacts	Mobile	+91 70119 55193
+G78	G78							* myContacts	Mobile	+91 70114 89984
+G79	G79							* myContacts	Mobile	+91 70009 25606
+G80	G80							* myContacts	Mobile	+91 63989 06170
+G81	G81						https://lh3.googleusercontent.com/contacts/AG6tpzFsX7xOJ4KTI_fHQbkJomVTFw5UnUdKEijKcm-o4ZXNEuChypzL	* myContacts	Mobile	+91 63773 77324
+G82	G82							* myContacts	Mobile	+91 63769 00606
+G83	G83							* myContacts	Mobile	+91 63760 18286
+G84	G84							* myContacts	Mobile	+91 63671 43035
+G85	G85							* myContacts	Mobile	+91 63505 50998
+G86	G86							* myContacts	Mobile	+91 63505 21097
+G87	G87							* myContacts	Mobile	+91 6295 992 194
+G88	G88							* myContacts	Mobile	+91 62833 80332
+G89	G89							* myContacts	Mobile	+91 62808 50571
+G90	G90							* myContacts	Mobile	+91 62803 15082
+G91	G91							* myContacts	Mobile	+91 62632 50235
+G93	G93							* myContacts	Mobile	+91 62394 76321
+G94	G94							* myContacts	Mobile	+91 83021 39897
+G95	G95							* myContacts	Mobile	+91 82958 07521
+G96	G96							* myContacts	Mobile	+91 82851 89085
+G97	G97							* myContacts	Mobile	+91 82840 02970
+G98	G98							* myContacts	Mobile	+91 82795 79659
+G99	G99							* myContacts	Mobile	+91 82737 97185
+Gamer 1	Gamer 1							* myContacts	Mobile	79008 01316
+Gamer 2	Gamer 2							* myContacts	Mobile	84372 72071
+Gamer 3	Gamer 3							* myContacts	Mobile	84374 78879
+Gamer 4	Gamer 4							* myContacts	Mobile	84472 93847
+Gamer 5	Gamer 5							* myContacts	Mobile	84610 58011
+Gamer 6	Gamer 6							* myContacts	Mobile	85589 26763
+Gamer 7	Gamer 7							* myContacts	Mobile	85590 50918
+Gamer 8	Gamer 8							* myContacts	Mobile	85710 27329
+Gamer 9	Gamer 9							* myContacts	Mobile	85868 52634
+Sanjay	Sanjay 							* myContacts	Mobile	+91 80594 11634
+Sitaram Bhai	Sitaram Bhai 							* myContacts	Mobile	+91 89509 00561 ::: 89509 00561
+V161	V161							* myContacts	Mobile	+91 83094 96388
+V177	V177							* myContacts	Mobile	+91 82754 04586
+V228	V228							* myContacts	Mobile	+91 79746 61120
+V255	V255							* myContacts	Mobile	+91 63626 25375
+V302	V302							* myContacts	Mobile	+91 72249 20591
+V335	V335							* myContacts	Mobile	+91 72628 58627
